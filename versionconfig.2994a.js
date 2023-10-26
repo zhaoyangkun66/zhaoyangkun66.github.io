6586b24fb4ce7850ea181a8362caccebcae1abe4
zhaoyangkun66 <569188417@qq.com>
2023-10-26 11:20:15 +0800

    1

---
+++ versionconfig.2994a.js
@@ -0,0 +1,9 @@
+window.g_cfg = {
+	cssName: 'style-mobile.25fc5.css',
+	icoName: 'favicon.02b7a.ico',
+	appleIcoName: 'favicon.532b8.png',
+	settingName: 'src/settings.c46aa.js',
+	mainName: 'main.6c7c9.js',
+	cocos2djsName: 'cocos2d-js-min.0b401.js',
+	physicsName: 'physics-min.js'
+};
--- versionconfig.3265f.js
+++
@@ -1,9 +0,0 @@
-window.g_cfg = {
-	cssName: 'style-mobile.25fc5.css',
-	icoName: 'favicon.dd26f.ico',
-	appleIcoName: 'favicon.f7ca3.png',
-	settingName: 'src/settings.82da9.js',
-	mainName: 'main.eaa25.js',
-	cocos2djsName: 'cocos2d-js-min.86b63.js',
-	physicsName: 'physics-min.js'
-};
