c5e92af1434583672ae2726fdd7f96039a542bd4
zhaoyangkun66 <569188417@qq.com>
2023-11-02 15:15:41 +0800

    1

--- versionconfig.e26cf.js
+++ versionconfig.e26cf.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.02b7a.ico',
 	appleIcoName: 'favicon.532b8.png',
-	settingName: 'src/settings.b8976.js',
-	mainName: 'main.23c86.js',
+	settingName: 'src/settings.bd330.js',
+	mainName: 'main.e1303.js',
 	cocos2djsName: 'cocos2d-js-min.aaca9.js',
 	physicsName: 'physics-min.js'
 };
