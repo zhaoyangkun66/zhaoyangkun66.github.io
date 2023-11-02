5058882837145a2b209b75c8af6e27041901f038
zhaoyangkun66 <569188417@qq.com>
2023-11-02 16:16:10 +0800

    1

--- versionconfig.20bbc.js
+++ versionconfig.20bbc.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.02b7a.ico',
 	appleIcoName: 'favicon.532b8.png',
-	settingName: 'src/settings.bd330.js',
-	mainName: 'main.e1303.js',
+	settingName: 'src/settings.6c8bc.js',
+	mainName: 'main.9f155.js',
 	cocos2djsName: 'cocos2d-js-min.aaca9.js',
 	physicsName: 'physics-min.js'
 };
