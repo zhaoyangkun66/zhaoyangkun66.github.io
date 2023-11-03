71c1362ea422d53c49e978eebf14243e3179e5a0
zhaoyangkun66 <569188417@qq.com>
2023-11-03 16:10:34 +0800

    1

--- versionconfig.d21e6.js
+++ versionconfig.d21e6.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.02b7a.ico',
 	appleIcoName: 'favicon.532b8.png',
-	settingName: 'src/settings.4adaf.js',
-	mainName: 'main.3236a.js',
+	settingName: 'src/settings.667c8.js',
+	mainName: 'main.4060a.js',
 	cocos2djsName: 'cocos2d-js-min.aaca9.js',
 	physicsName: 'physics-min.js'
 };
