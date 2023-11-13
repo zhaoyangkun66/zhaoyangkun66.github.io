406997f1c3732f07f42123bbaaa494334873646c
zhaoyangkun66 <569188417@qq.com>
2023-11-13 14:14:10 +0800

    1

--- versionconfig.b469d.js
+++ versionconfig.b469d.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.bd553.ico',
 	appleIcoName: 'favicon.5d155.png',
-	settingName: 'src/settings.a8044.js',
-	mainName: 'main.5f8f8.js',
+	settingName: 'src/settings.11cac.js',
+	mainName: 'main.a5781.js',
 	cocos2djsName: 'cocos2d-js-min.aaca9.js',
 	physicsName: 'physics-min.js'
 };
