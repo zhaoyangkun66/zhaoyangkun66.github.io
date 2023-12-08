af62c5bb5edcf167e59713354877957cf4304941
zhaoyangkun66 <569188417@qq.com>
2023-12-08 18:11:23 +0800

    1

--- versionconfig.db4e2.js
+++ versionconfig.db4e2.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.bd553.ico',
 	appleIcoName: 'favicon.5d155.png',
-	settingName: 'src/settings.7c41a.js',
-	mainName: 'main.14fa9.js',
+	settingName: 'src/settings.0a892.js',
+	mainName: 'main.c7800.js',
 	cocos2djsName: 'cocos2d-js-min.c65eb.js',
 	physicsName: 'physics-min.js'
 };
