773cf97440892abeb3fa85005698f9974904492e
zhaoyangkun66 <569188417@qq.com>
2024-01-09 19:29:30 +0800

    1

--- versionconfig.ae3f3.js
+++ versionconfig.ae3f3.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.bd553.ico',
 	appleIcoName: 'favicon.5d155.png',
-	settingName: 'src/settings.d1bb5.js',
-	mainName: 'main.921db.js',
+	settingName: 'src/settings.021a4.js',
+	mainName: 'main.d57cc.js',
 	cocos2djsName: 'cocos2d-js-min.c65eb.js',
 	physicsName: 'physics-min.js'
 };
