cd7ae871a6849a7b8ea0d5433382d7dcee487394
zhaoyangkun66 <569188417@qq.com>
2024-01-17 18:16:07 +0800

    1

--- test/web-mobile/versionconfig.51fb9.js
+++
@@ -1,9 +0,0 @@
-window.g_cfg = {
-	cssName: 'style-mobile.25fc5.css',
-	icoName: 'favicon.bd553.ico',
-	appleIcoName: 'favicon.5d155.png',
-	settingName: 'src/settings.90dfe.js',
-	mainName: 'main.aad53.js',
-	cocos2djsName: 'cocos2d-js-min.c65eb.js',
-	physicsName: 'physics-min.js'
-};
---
+++ test/web-mobile/versionconfig.6258f.js
@@ -0,0 +1,9 @@
+window.g_cfg = {
+	cssName: 'style-mobile.25fc5.css',
+	icoName: 'favicon.bd553.ico',
+	appleIcoName: 'favicon.5d155.png',
+	settingName: 'src/settings.de045.js',
+	mainName: 'main.01ff0.js',
+	cocos2djsName: 'cocos2d-js-min.c65eb.js',
+	physicsName: 'physics-min.js'
+};
