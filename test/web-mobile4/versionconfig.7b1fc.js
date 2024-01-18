b37397add34fe57a7aaca2d2207c98a65f87417c
zhaoyangkun66 <569188417@qq.com>
2024-01-18 10:53:43 +0800

    1

--- test/web-mobile4/versionconfig.7b1fc.js
+++ test/web-mobile4/versionconfig.7b1fc.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.bd553.ico',
 	appleIcoName: 'favicon.5d155.png',
-	settingName: 'src/settings.de045.js',
-	mainName: 'main.01ff0.js',
+	settingName: 'src/settings.72fba.js',
+	mainName: 'main.de29a.js',
 	cocos2djsName: 'cocos2d-js-min.c65eb.js',
 	physicsName: 'physics-min.js'
 };
