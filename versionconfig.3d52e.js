daf1f96bf43fd8496d2554e6b3f7a1993e8f301d
zhaoyangkun66 <569188417@qq.com>
2024-02-04 15:35:09 +0800

    1

---
+++ versionconfig.3d52e.js
@@ -0,0 +1,9 @@
+window.g_cfg = {
+	cssName: 'style-mobile.25fc5.css',
+	icoName: 'favicon.bd553.ico',
+	appleIcoName: 'favicon.5d155.png',
+	settingName: 'src/settings.f6150.js',
+	mainName: 'main.2e8ee.js',
+	cocos2djsName: 'cocos2d-js-min.133a8.js',
+	physicsName: 'physics-min.js'
+};
--- versionconfig.94e9e.js
+++
@@ -1,9 +0,0 @@
-window.g_cfg = {
-	cssName: 'style-mobile.25fc5.css',
-	icoName: 'favicon.bd553.ico',
-	appleIcoName: 'favicon.5d155.png',
-	settingName: 'src/settings.6c270.js',
-	mainName: 'main.8519c.js',
-	cocos2djsName: 'cocos2d-js-min.133a8.js',
-	physicsName: 'physics-min.js'
-};
