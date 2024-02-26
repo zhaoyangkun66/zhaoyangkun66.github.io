3c4548ce8331364385bdc0713656765cc47a28bf
zhaoyangkun66 <569188417@qq.com>
2024-02-26 18:57:50 +0800

    1

--- columbia/versionconfig.88c17.js
+++ columbia/versionconfig.88c17.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.bd553.ico',
 	appleIcoName: 'favicon.5d155.png',
-	settingName: 'src/settings.a106e.js',
-	mainName: 'main.05f24.js',
+	settingName: 'src/settings.2a5d8.js',
+	mainName: 'main.109af.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
