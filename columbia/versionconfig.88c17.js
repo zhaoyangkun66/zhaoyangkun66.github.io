42d23d1ec697dbe62c6004488f8b856b87423391
zhaoyangkun66 <569188417@qq.com>
2024-02-28 17:19:46 +0800

    1

--- columbia/versionconfig.88c17.js
+++
@@ -1,9 +0,0 @@
-window.g_cfg = {
-	cssName: 'style-mobile.25fc5.css',
-	icoName: 'favicon.bd553.ico',
-	appleIcoName: 'favicon.5d155.png',
-	settingName: 'src/settings.2a5d8.js',
-	mainName: 'main.109af.js',
-	cocos2djsName: 'cocos2d-js-min.133a8.js',
-	physicsName: 'physics-min.js'
-};
---
+++ columbia/versionconfig.f946d.js
@@ -0,0 +1,9 @@
+window.g_cfg = {
+	cssName: 'style-mobile.25fc5.css',
+	icoName: 'favicon.293a8.ico',
+	appleIcoName: 'favicon.17542.png',
+	settingName: 'src/settings.d7abf.js',
+	mainName: 'main.754f8.js',
+	cocos2djsName: 'cocos2d-js-min.133a8.js',
+	physicsName: 'physics-min.js'
+};
