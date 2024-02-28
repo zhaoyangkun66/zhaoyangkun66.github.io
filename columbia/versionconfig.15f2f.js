70023cc2f52434d1ed12820a32273822ed88ec47
zhaoyangkun66 <569188417@qq.com>
2024-02-28 19:06:03 +0800

    1

--- columbia/versionconfig.15f2f.js
+++ columbia/versionconfig.15f2f.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.293a8.ico',
 	appleIcoName: 'favicon.35976.png',
-	settingName: 'src/settings.c692c.js',
-	mainName: 'main.bed16.js',
+	settingName: 'src/settings.a1812.js',
+	mainName: 'main.365de.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
