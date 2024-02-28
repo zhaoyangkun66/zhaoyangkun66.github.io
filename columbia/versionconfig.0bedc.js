7e9e2c0c102b2878757890c65515b0a269083c7f
zhaoyangkun66 <569188417@qq.com>
2024-02-28 17:46:36 +0800

    1

--- columbia/versionconfig.0bedc.js
+++ columbia/versionconfig.0bedc.js
@@ -1,7 +1,7 @@
 window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.293a8.ico',
-	appleIcoName: 'favicon.17542.png',
+	appleIcoName: 'favicon.35976.png',
 	settingName: 'src/settings.d7abf.js',
 	mainName: 'main.754f8.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
