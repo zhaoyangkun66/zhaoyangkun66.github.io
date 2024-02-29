bdf51d870981b4c92dc9ac291f8a67c94ee10098
zhaoyangkun66 <569188417@qq.com>
2024-02-29 13:50:18 +0800

    1

--- columbia/versionconfig.dda8e.js
+++ columbia/versionconfig.dda8e.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.293a8.ico',
 	appleIcoName: 'favicon.35976.png',
-	settingName: 'src/settings.a3d7e.js',
-	mainName: 'main.57c12.js',
+	settingName: 'src/settings.88860.js',
+	mainName: 'main.a5627.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
