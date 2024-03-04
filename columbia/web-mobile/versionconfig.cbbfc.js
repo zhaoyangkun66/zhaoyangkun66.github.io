a88043b4c18403c48524ba823d57efff0276d805
zhaoyangkun66 <569188417@qq.com>
2024-03-04 16:38:11 +0800

    1

--- columbia/web-mobile/versionconfig.cbbfc.js
+++ columbia/web-mobile/versionconfig.cbbfc.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.293a8.ico',
 	appleIcoName: 'favicon.35976.png',
-	settingName: 'src/settings.578d3.js',
-	mainName: 'main.0fabe.js',
+	settingName: 'src/settings.94669.js',
+	mainName: 'main.6f700.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
