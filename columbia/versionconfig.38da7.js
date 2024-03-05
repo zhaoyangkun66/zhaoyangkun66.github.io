07400882d90e5c694fbc7f1fb184dbb0e983c5df
zhaoyangkun66 <569188417@qq.com>
2024-03-05 10:47:10 +0800

    1

--- columbia/versionconfig.38da7.js
+++
@@ -1,9 +0,0 @@
-window.g_cfg = {
-	cssName: 'style-mobile.25fc5.css',
-	icoName: 'favicon.293a8.ico',
-	appleIcoName: 'favicon.35976.png',
-	settingName: 'src/settings.f0260.js',
-	mainName: 'main.a71f2.js',
-	cocos2djsName: 'cocos2d-js-min.133a8.js',
-	physicsName: 'physics-min.js'
-};
