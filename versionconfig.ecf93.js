7d9b89a33d3de1b6993ef4a42954e8f670ed74fb
zhaoyangkun66 <569188417@qq.com>
2024-04-23 14:10:03 +0800

    1

--- versionconfig.ecf93.js
+++ versionconfig.ecf93.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.293a8.ico',
 	appleIcoName: 'favicon.35976.png',
-	settingName: 'src/settings.1b94b.js',
-	mainName: 'main.c14ee.js',
+	settingName: 'src/settings.2f1e3.js',
+	mainName: 'main.054da.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
