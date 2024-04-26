ceaecea45b5be062d1b241c19f3f6303e2700a0d
zhaoyangkun66 <569188417@qq.com>
2024-04-26 19:45:20 +0800

    1

--- 2048bet/versionconfig.8c0d4.js
+++
@@ -1,9 +0,0 @@
-window.g_cfg = {
-	cssName: 'style-mobile.25fc5.css',
-	icoName: 'favicon.293a8.ico',
-	appleIcoName: 'favicon.35976.png',
-	settingName: 'src/settings.def07.js',
-	mainName: 'main.94c82.js',
-	cocos2djsName: 'cocos2d-js-min.133a8.js',
-	physicsName: 'physics-min.js'
-};
