1ee16a3bd1f85170d1a67288f20d465b9507b785
zhaoyangkun66 <569188417@qq.com>
2024-06-03 17:42:13 +0800

    1

--- versionconfig.626bd.js
+++
@@ -1,9 +0,0 @@
-window.g_cfg = {
-	cssName: 'style-mobile.25fc5.css',
-	icoName: 'favicon.293a8.ico',
-	appleIcoName: 'favicon.35976.png',
-	settingName: 'src/settings.6bdb6.js',
-	mainName: 'main.1398d.js',
-	cocos2djsName: 'cocos2d-js-min.133a8.js',
-	physicsName: 'physics-min.js'
-};
