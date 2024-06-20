7095d7a912556cc238e40d81281998beb39727ac
zhaoyangkun66 <569188417@qq.com>
2024-06-20 10:44:51 +0800

    1

--- 2048bet/versionconfig.d8129.js
+++ 2048bet/versionconfig.d8129.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.a2d59.ico',
 	appleIcoName: 'favicon.312be.png',
-	settingName: 'src/settings.75e25.js',
-	mainName: 'main.bdc33.js',
+	settingName: 'src/settings.e75a7.js',
+	mainName: 'main.ac84b.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
