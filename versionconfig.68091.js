c33323720a184a9fcb51477732e7d0e7e8548c6f
zhaoyangkun66 <569188417@qq.com>
2024-06-26 18:19:47 +0800

    1

--- versionconfig.68091.js
+++ versionconfig.68091.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.293a8.ico',
 	appleIcoName: 'favicon.35976.png',
-	settingName: 'src/settings.3b857.js',
-	mainName: 'main.bda10.js',
+	settingName: 'src/settings.79e33.js',
+	mainName: 'main.015a8.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
