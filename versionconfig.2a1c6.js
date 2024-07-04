92a4ad7dca64c880469546abb2615186774e7ba4
zhaoyangkun66 <569188417@qq.com>
2024-07-04 17:08:41 +0800

    1

--- versionconfig.2a1c6.js
+++ versionconfig.2a1c6.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.293a8.ico',
 	appleIcoName: 'favicon.35976.png',
-	settingName: 'src/settings.79e33.js',
-	mainName: 'main.015a8.js',
+	settingName: 'src/settings.1cb08.js',
+	mainName: 'main.e2c73.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
