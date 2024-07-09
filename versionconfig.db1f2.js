100da02b1cdc7dbb0c69fd7176967d815a1c0b4e
zhaoyangkun66 <569188417@qq.com>
2024-07-09 13:52:13 +0800

    1

--- versionconfig.db1f2.js
+++ versionconfig.db1f2.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.293a8.ico',
 	appleIcoName: 'favicon.35976.png',
-	settingName: 'src/settings.1b5bd.js',
-	mainName: 'main.d6be3.js',
+	settingName: 'src/settings.cc270.js',
+	mainName: 'main.5456f.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
