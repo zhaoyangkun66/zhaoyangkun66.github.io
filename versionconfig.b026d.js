7aeeb605a47e0e1855d5c211fbb5f44715035acf
zhaoyangkun66 <569188417@qq.com>
2024-07-22 14:41:09 +0800

    1

---
+++ versionconfig.b026d.js
@@ -0,0 +1,9 @@
+window.g_cfg = {
+	cssName: 'style-mobile.25fc5.css',
+	icoName: 'favicon.293a8.ico',
+	appleIcoName: 'favicon.35976.png',
+	settingName: 'src/settings.f3a22.js',
+	mainName: 'main.b0342.js',
+	cocos2djsName: 'cocos2d-js-min.133a8.js',
+	physicsName: 'physics-min.js'
+};
