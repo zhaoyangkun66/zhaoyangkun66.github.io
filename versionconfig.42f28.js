f2f9e86e002a1bd4833030211f93f003c8be23dc
zhaoyangkun66 <569188417@qq.com>
2024-08-13 16:41:35 +0800

    1

---
+++ versionconfig.42f28.js
@@ -0,0 +1,9 @@
+window.g_cfg = {
+	cssName: 'style-mobile.25fc5.css',
+	icoName: 'favicon.293a8.ico',
+	appleIcoName: 'favicon.3a27a.png',
+	settingName: 'src/settings.2c7c5.js',
+	mainName: 'main.29487.js',
+	cocos2djsName: 'cocos2d-js-min.133a8.js',
+	physicsName: 'physics-min.js'
+};
