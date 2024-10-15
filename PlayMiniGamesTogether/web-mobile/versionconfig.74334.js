5fd7bc118ab922191ab3ca6a031b93cb1423e6c5
zhaoyangkun66 <569188417@qq.com>
2024-10-15 11:03:10 +0800

    1

--- PlayMiniGamesTogether/web-mobile/versionconfig.74334.js
+++ PlayMiniGamesTogether/web-mobile/versionconfig.74334.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.b8c3e.ico',
 	appleIcoName: 'favicon.png',
-	settingName: 'src/settings.339a9.js',
-	mainName: 'main.c0140.js',
+	settingName: 'src/settings.06d63.js',
+	mainName: 'main.00d2d.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
