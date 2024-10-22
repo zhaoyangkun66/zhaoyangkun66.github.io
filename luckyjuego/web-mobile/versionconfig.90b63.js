29de7f81dd1e0dda2c9f2eb4e59dab3f77d72dc2
zhaoyangkun66 <569188417@qq.com>
2024-10-22 14:22:15 +0800

    1

--- luckyjuego/web-mobile/versionconfig.90b63.js
+++ luckyjuego/web-mobile/versionconfig.90b63.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.840ba.ico',
 	appleIcoName: 'favicon.ec1a4.png',
-	settingName: 'src/settings.d5113.js',
-	mainName: 'main.470ef.js',
+	settingName: 'src/settings.bbd56.js',
+	mainName: 'main.02eb2.js',
 	cocos2djsName: 'cocos2d-js-min.c65eb.js',
 	physicsName: 'physics-min.js'
 };
