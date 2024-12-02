08f7ebb1be8457454a38d9adc68bcdbb97934349
zhaoyangkun66 <569188417@qq.com>
2024-12-02 15:47:55 +0800

    1

--- 2048bet/versionconfig.4cf0e.js
+++ 2048bet/versionconfig.4cf0e.js
@@ -1,7 +1,7 @@
 window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
-	icoName: 'favicon.a2d59.ico',
-	appleIcoName: 'favicon.312be.png',
+	icoName: 'favicon.6726d.ico',
+	appleIcoName: 'favicon.d657c.png',
 	settingName: 'src/settings.49125.js',
 	mainName: 'main.46a97.js',
 	cocos2djsName: 'cocos2d-js-min.c65eb.js',
