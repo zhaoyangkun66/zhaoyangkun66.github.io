f515056239f759d3d0c6c3f52015a0d6f8d68349
zhaoyangkun66 <569188417@qq.com>
2025-01-15 13:05:18 +0800

    1

--- 2048bet/versionconfig.effdc.js
+++ 2048bet/versionconfig.effdc.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.6726d.ico',
 	appleIcoName: 'favicon.d657c.png',
-	settingName: 'src/settings.9cf46.js',
-	mainName: 'main.70a6a.js',
+	settingName: 'src/settings.85779.js',
+	mainName: 'main.075e8.js',
 	cocos2djsName: 'cocos2d-js-min.c65eb.js',
 	physicsName: 'physics-min.js'
 };
