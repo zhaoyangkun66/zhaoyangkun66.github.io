8a3a83774216172178186210ce6cc340cc64c433
zhaoyangkun66 <569188417@qq.com>
2025-01-16 18:05:37 +0800

    1

--- 2048bet/versionconfig.316f5.js
+++ 2048bet/versionconfig.316f5.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.6726d.ico',
 	appleIcoName: 'favicon.d657c.png',
-	settingName: 'src/settings.88b61.js',
-	mainName: 'main.5924f.js',
+	settingName: 'src/settings.f6bb7.js',
+	mainName: 'main.31721.js',
 	cocos2djsName: 'cocos2d-js-min.c65eb.js',
 	physicsName: 'physics-min.js'
 };
