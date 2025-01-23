bd26421ca160eca25ccbc2f544d8cff2b6dbbc65
zhaoyangkun66 <569188417@qq.com>
2025-01-23 18:44:36 +0800

    1

--- luckyjuego/web-mobile/client/GemKiller/application.65cf1.js
+++ luckyjuego/web-mobile/client/GemKiller/application.65cf1.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.dc70e.json';
+          this.settingsPath = 'src/settings.9c598.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
