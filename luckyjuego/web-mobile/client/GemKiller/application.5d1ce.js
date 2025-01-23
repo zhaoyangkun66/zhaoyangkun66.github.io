7b663bb74c9b3965584518539b00ae18191876ff
zhaoyangkun66 <569188417@qq.com>
2025-01-23 17:08:15 +0800

    2

--- luckyjuego/web-mobile/client/GemKiller/application.5d1ce.js
+++ luckyjuego/web-mobile/client/GemKiller/application.5d1ce.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.0ca8b.json';
+          this.settingsPath = 'src/settings.a67e9.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- luckyjuego/web-mobile/client/GemKiller/index.6109c.js
+++ luckyjuego/web-mobile/client/GemKiller/index.6109c.js
@@ -1,4 +1,4 @@
-System.register(["./application.30028.js"], function (_export, _context) {
+System.register(["./application.5d1ce.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
