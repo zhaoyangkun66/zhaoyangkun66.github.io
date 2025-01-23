7088be8039f84761bafd4e29077ce9e51a53664f
zhaoyangkun66 <569188417@qq.com>
2025-01-23 17:04:07 +0800

    1

--- client/GemKiller/application.5d1ce.js
+++ client/GemKiller/application.5d1ce.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.f2f8a.json';
+          this.settingsPath = 'src/settings.a67e9.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
