e1c3b0523ae1dbdc2f69c1224b0aecb970ced4da
zhaoyangkun66 <569188417@qq.com>
2025-01-24 13:01:55 +0800

    1

--- luckyjuego/web-mobile/client/GemKiller/index.07f9f.js
+++ luckyjuego/web-mobile/client/GemKiller/index.07f9f.js
@@ -1,4 +1,4 @@
-System.register(["./application.65cf1.js"], function (_export, _context) {
+System.register(["./application.ba8f8.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
