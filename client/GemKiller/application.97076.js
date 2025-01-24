a847bfeb4817e039a0ea8d6cabe04aed17afa8c5
zhaoyangkun66 <569188417@qq.com>
2025-01-24 14:27:29 +0800

    1

--- client/GemKiller/application.97076.js
+++ client/GemKiller/application.97076.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.ead06.json';
+          this.settingsPath = 'src/settings.5285a.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- client/GemKiller/index.fdb72.js
+++ client/GemKiller/index.fdb72.js
@@ -1,4 +1,4 @@
-System.register(["./application.2b703.js"], function (_export, _context) {
+System.register(["./application.97076.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
