ee1c94f113993b45206aa55ca9f7b5983f89a05f
zhaoyangkun66 <569188417@qq.com>
2025-01-24 14:25:57 +0800

    1

--- 2048bet/client/GemKiller/application.13d94.js
+++ 2048bet/client/GemKiller/application.13d94.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.48a6a.json';
+          this.settingsPath = 'src/settings.fe11d.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- 2048bet/client/GemKiller/index.7b1fa.js
+++ 2048bet/client/GemKiller/index.7b1fa.js
@@ -1,4 +1,4 @@
-System.register(["./application.557d2.js"], function (_export, _context) {
+System.register(["./application.13d94.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
