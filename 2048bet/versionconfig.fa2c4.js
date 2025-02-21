dad062ecfb07cccec6d8cfc4f42f0b56fcdc4671
zhaoyangkun66 <569188417@qq.com>
2025-02-21 17:56:38 +0800

    1

--- 2048bet/versionconfig.fa2c4.js
+++ 2048bet/versionconfig.fa2c4.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.6726d.ico',
 	appleIcoName: 'favicon.d657c.png',
-	settingName: 'src/settings.bf411.js',
-	mainName: 'main.dc731.js',
+	settingName: 'src/settings.be473.js',
+	mainName: 'main.ee224.js',
 	cocos2djsName: 'cocos2d-js-min.c65eb.js',
 	physicsName: 'physics-min.js'
 };
