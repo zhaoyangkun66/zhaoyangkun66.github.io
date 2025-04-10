46632fa937c39aac82165e9013aef970e35748d6
zhaoyangkun66 <569188417@qq.com>
2025-04-10 14:11:13 +0800

    1

--- 2048bet/versionconfig.50add.js
+++ 2048bet/versionconfig.50add.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.6726d.ico',
 	appleIcoName: 'favicon.d657c.png',
-	settingName: 'src/settings.6998d.js',
-	mainName: 'main.43cb8.js',
+	settingName: 'src/settings.76027.js',
+	mainName: 'main.de959.js',
 	cocos2djsName: 'cocos2d-js-min.c65eb.js',
 	physicsName: 'physics-min.js'
 };
