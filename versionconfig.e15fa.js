c52054bc6a0fe8bbbac5663e1676b758cf3108c6
zhaoyangkun66 <569188417@qq.com>
2025-04-14 13:23:07 +0800

    1

--- versionconfig.e15fa.js
+++ versionconfig.e15fa.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.293a8.ico',
 	appleIcoName: 'favicon.3a27a.png',
-	settingName: 'src/settings.10769.js',
-	mainName: 'main.9ff7a.js',
+	settingName: 'src/settings.08fcf.js',
+	mainName: 'main.ea1ff.js',
 	cocos2djsName: 'cocos2d-js-min.c65eb.js',
 	physicsName: 'physics-min.js'
 };
