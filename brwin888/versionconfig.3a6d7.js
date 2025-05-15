6c5090cb69e639bbe670f1e81a4df0d64fe1a534
zhaoyangkun66 <569188417@qq.com>
2025-05-15 13:10:43 +0800

    1

--- brwin888/versionconfig.3a6d7.js
+++
@@ -1,9 +0,0 @@
-window.g_cfg = {
-	cssName: 'style-mobile.25fc5.css',
-	icoName: 'favicon.6726d.ico',
-	appleIcoName: 'favicon.d657c.png',
-	settingName: 'src/settings.d9193.js',
-	mainName: 'main.cfc1d.js',
-	cocos2djsName: 'cocos2d-js-min.3a4eb.js',
-	physicsName: 'physics-min.js'
-};
---
+++ brwin888/versionconfig.55cef.js
@@ -0,0 +1,9 @@
+window.g_cfg = {
+	cssName: 'style-mobile.25fc5.css',
+	icoName: 'favicon.e46ce.ico',
+	appleIcoName: 'favicon.f71a0.png',
+	settingName: 'src/settings.02de7.js',
+	mainName: 'main.cb9a3.js',
+	cocos2djsName: 'cocos2d-js-min.758cf.js',
+	physicsName: 'physics-min.js'
+};
