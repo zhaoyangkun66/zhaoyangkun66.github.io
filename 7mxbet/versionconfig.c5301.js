28c53fd820a6fef9c7c79974a614839d87f88a7f
zhaoyangkun66 <569188417@qq.com>
2025-05-22 17:10:40 +0800

    1

---
+++ 7mxbet/versionconfig.c5301.js
@@ -0,0 +1,9 @@
+window.g_cfg = {
+	cssName: 'style-mobile.25fc5.css',
+	icoName: 'favicon.840ba.ico',
+	appleIcoName: 'favicon.a06a2.png',
+	settingName: 'src/settings.884f8.js',
+	mainName: 'main.0c071.js',
+	cocos2djsName: 'cocos2d-js-min.133a8.js',
+	physicsName: 'physics-min.js'
+};
