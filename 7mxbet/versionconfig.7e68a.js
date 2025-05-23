9723d35eb05404fe4898334a44ff4eeb1e739c0e
zhaoyangkun66 <569188417@qq.com>
2025-05-23 15:59:24 +0800

    1

--- 7mxbet/versionconfig.7e68a.js
+++
@@ -1,9 +0,0 @@
-window.g_cfg = {
-	cssName: 'style-mobile.25fc5.css',
-	icoName: 'favicon.840ba.ico',
-	appleIcoName: 'favicon.a06a2.png',
-	settingName: 'src/settings.150dd.js',
-	mainName: 'main.0a6b9.js',
-	cocos2djsName: 'cocos2d-js-min.d3e04.js',
-	physicsName: 'physics-min.js'
-};
---
+++ 7mxbet/versionconfig.91ca1.js
@@ -0,0 +1,9 @@
+window.g_cfg = {
+	cssName: 'style-mobile.25fc5.css',
+	icoName: 'favicon.63b85.ico',
+	appleIcoName: 'favicon.png',
+	settingName: 'src/settings.1379a.js',
+	mainName: 'main.c7572.js',
+	cocos2djsName: 'cocos2d-js-min.d3e04.js',
+	physicsName: 'physics-min.js'
+};
