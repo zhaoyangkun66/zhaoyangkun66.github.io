4d709d2a6f50b4a54969e7f713ce9a4da508cf69
zhaoyangkun66 <569188417@qq.com>
2025-05-23 13:07:23 +0800

    1

--- 7mxbet/versionconfig.7e68a.js
+++ 7mxbet/versionconfig.7e68a.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.840ba.ico',
 	appleIcoName: 'favicon.a06a2.png',
-	settingName: 'src/settings.bb432.js',
-	mainName: 'main.c94ec.js',
-	cocos2djsName: 'cocos2d-js-min.133a8.js',
+	settingName: 'src/settings.150dd.js',
+	mainName: 'main.0a6b9.js',
+	cocos2djsName: 'cocos2d-js-min.d3e04.js',
 	physicsName: 'physics-min.js'
 };
