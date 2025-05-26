cf15520bbcb16583f25c58651c7294d2c46208d4
zhaoyangkun66 <569188417@qq.com>
2025-05-26 10:39:46 +0800

    1

--- 7mxbet/versionconfig.25195.js
+++ 7mxbet/versionconfig.25195.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.63b85.ico',
 	appleIcoName: 'favicon.png',
-	settingName: 'src/settings.c7357.js',
-	mainName: 'main.0e4be.js',
+	settingName: 'src/settings.d6f26.js',
+	mainName: 'main.c788d.js',
 	cocos2djsName: 'cocos2d-js-min.d3e04.js',
 	physicsName: 'physics-min.js'
 };
