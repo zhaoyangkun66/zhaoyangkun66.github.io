b1598202067d049dc2f035a46be377926fccb7a3
zhaoyangkun66 <569188417@qq.com>
2025-06-03 10:59:26 +0800

    1

--- 7mxbet/versionconfig.cf810.js
+++ 7mxbet/versionconfig.cf810.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.63b85.ico',
 	appleIcoName: 'favicon.png',
-	settingName: 'src/settings.d8450.js',
-	mainName: 'main.453ad.js',
+	settingName: 'src/settings.be73b.js',
+	mainName: 'main.7dfe8.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
