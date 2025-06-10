12b4a57e825305aaa4979b7c4955928e1a3ab2c7
zhaoyangkun66 <569188417@qq.com>
2025-06-10 18:45:18 +0800

    1

--- brwin888/versionconfig.084db.js
+++ brwin888/versionconfig.084db.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.e46ce.ico',
 	appleIcoName: 'favicon.f71a0.png',
-	settingName: 'src/settings.b9b4d.js',
-	mainName: 'main.4a185.js',
+	settingName: 'src/settings.29209.js',
+	mainName: 'main.33fa3.js',
 	cocos2djsName: 'cocos2d-js-min.3a4eb.js',
 	physicsName: 'physics-min.js'
 };
