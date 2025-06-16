35fb5dd5ed5aa0914248772aa84bca1f2a10349d
zhaoyangkun66 <569188417@qq.com>
2025-06-16 18:48:09 +0800

    1

--- brwin888/versionconfig.433be.js
+++ brwin888/versionconfig.433be.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.e46ce.ico',
 	appleIcoName: 'favicon.f71a0.png',
-	settingName: 'src/settings.1aea1.js',
-	mainName: 'main.9af32.js',
+	settingName: 'src/settings.7cd34.js',
+	mainName: 'main.88040.js',
 	cocos2djsName: 'cocos2d-js-min.3a4eb.js',
 	physicsName: 'physics-min.js'
 };
