c5cf6d408b05b25fa06e33af799d280119e2fbf0
zhaoyangkun66 <569188417@qq.com>
2025-06-25 17:21:13 +0800

    1

--- brwin888/versionconfig.3f809.js
+++ brwin888/versionconfig.3f809.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.e46ce.ico',
 	appleIcoName: 'favicon.f71a0.png',
-	settingName: 'src/settings.32ed4.js',
-	mainName: 'main.924b0.js',
-	cocos2djsName: 'cocos2d-js-min.3a4eb.js',
+	settingName: 'src/settings.cce92.js',
+	mainName: 'main.418fb.js',
+	cocos2djsName: 'cocos2d-js-min.f589e.js',
 	physicsName: 'physics-min.js'
 };
--- versionconfig.a4123.js
+++ versionconfig.a4123.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.293a8.ico',
 	appleIcoName: 'favicon.3a27a.png',
-	settingName: 'src/settings.66cfd.js',
-	mainName: 'main.1f336.js',
-	cocos2djsName: 'cocos2d-js-min.65fcc.js',
+	settingName: 'src/settings.44288.js',
+	mainName: 'main.ee167.js',
+	cocos2djsName: 'cocos2d-js-min.9d426.js',
 	physicsName: 'physics-min.js'
 };
