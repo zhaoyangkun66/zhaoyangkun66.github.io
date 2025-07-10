3c201553293bc42480bafc449538413f43f5cb93
zhaoyangkun66 <569188417@qq.com>
2025-07-10 17:47:14 +0800

    1

--- cljuego/versionconfig.e62c0.js
+++ cljuego/versionconfig.e62c0.js
@@ -1,9 +1,9 @@
 window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.914ff.ico',
-	appleIcoName: 'favicon.629af.png',
-	settingName: 'src/settings.319e0.js',
-	mainName: 'main.92871.js',
+	appleIcoName: 'favicon.605f3.png',
+	settingName: 'src/settings.ac55d.js',
+	mainName: 'main.bae79.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
