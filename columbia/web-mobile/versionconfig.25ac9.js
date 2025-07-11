32b532fa115ef1c42799c6ec431518592c7c7de5
zhaoyangkun66 <569188417@qq.com>
2025-07-11 17:32:59 +0800

    1

--- columbia/web-mobile/versionconfig.25ac9.js
+++
@@ -1,9 +0,0 @@
-window.g_cfg = {
-	cssName: 'style-mobile.25fc5.css',
-	icoName: 'favicon.293a8.ico',
-	appleIcoName: 'favicon.35976.png',
-	settingName: 'src/settings.a5d79.js',
-	mainName: 'main.4c4a3.js',
-	cocos2djsName: 'cocos2d-js-min.133a8.js',
-	physicsName: 'physics-min.js'
-};
