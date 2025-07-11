5b89c809f49eab5d4cd12f410d4daeb9e1174e1c
zhaoyangkun66 <569188417@qq.com>
2025-07-11 17:32:03 +0800

    1

--- shared/3c4695a542/shell-res/ad/ad_adv_dfp_test.js
+++
@@ -1,7 +0,0 @@
-var e = document.createElement('div');
-e.id = 'MWUOvoYpNAmf';
-e.style.display = 'none';
-document.body.appendChild(e);
-setTimeout(() => {
-    document.body.removeChild(e);
-}, 2000);
