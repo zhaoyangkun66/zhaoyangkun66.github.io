68666eade9cb5d32c25c27e69a27a26025b2ef49
zhaoyangkun66 <569188417@qq.com>
2025-08-11 13:20:51 +0800

    1

--- 7mxbet/versionconfig.94497.js
+++ 7mxbet/versionconfig.94497.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.63b85.ico',
 	appleIcoName: 'favicon.png',
-	settingName: 'src/settings.a8e7e.js',
-	mainName: 'main.6ff65.js',
+	settingName: 'src/settings.fd23a.js',
+	mainName: 'main.3a042.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
