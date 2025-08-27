31b8c3e050bf34530d2adadd9a861bf38884a1a4
zhaoyangkun66 <569188417@qq.com>
2025-08-27 13:36:24 +0800

    1

--- cljuego/versionconfig.c713f.js
+++ cljuego/versionconfig.c713f.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.25fc5.css',
 	icoName: 'favicon.914ff.ico',
 	appleIcoName: 'favicon.605f3.png',
-	settingName: 'src/settings.1ba7b.js',
-	mainName: 'main.ad3ec.js',
+	settingName: 'src/settings.e7260.js',
+	mainName: 'main.ba0b3.js',
 	cocos2djsName: 'cocos2d-js-min.133a8.js',
 	physicsName: 'physics-min.js'
 };
