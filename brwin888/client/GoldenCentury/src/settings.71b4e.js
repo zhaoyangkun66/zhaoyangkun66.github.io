fbb84e4328daaaf4d9bddf9172af009a5aaa1c52
zhaoyangkun66 <569188417@qq.com>
2026-01-28 16:10:50 +0800

    1

---
+++ brwin888/client/GoldenCentury/src/settings.71b4e.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/LoadingScene.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"ad5d2",resources_C:"f57c6",resources_H:"97257",resources_V:"ac2dc",main:"b1ad1"}};
