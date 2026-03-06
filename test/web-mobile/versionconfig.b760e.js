fda8794c856c4820ff3df8ed0e054b63e6d16296
zhaoyangkun66 <569188417@qq.com>
2026-03-06 14:09:30 +0800

    1

--- test/web-mobile/versionconfig.b760e.js
+++ test/web-mobile/versionconfig.b760e.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.a567e.css',
 	icoName: 'favicon.293a8.ico',
 	appleIcoName: 'favicon.3a27a.png',
-	settingName: 'src/settings.1399f.js',
-	mainName: 'main.b3bb4.js',
-	cocos2djsName: 'cocos2d-js-min.95aff.js',
+	settingName: 'src/settings.2fd7b.js',
+	mainName: 'main.e9b51.js',
+	cocos2djsName: 'cocos2d-js-min.4aff4.js',
 	physicsName: 'physics-min.js'
 };
