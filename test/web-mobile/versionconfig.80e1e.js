c00e9d15eebfb4066f3140facbaf7836962b75d1
zhaoyangkun66 <569188417@qq.com>
2026-03-19 17:38:29 +0800

    1

--- test/web-mobile/versionconfig.80e1e.js
+++ test/web-mobile/versionconfig.80e1e.js
@@ -2,8 +2,8 @@ window.g_cfg = {
 	cssName: 'style-mobile.a567e.css',
 	icoName: 'favicon.293a8.ico',
 	appleIcoName: 'favicon.3a27a.png',
-	settingName: 'src/settings.d4d56.js',
-	mainName: 'main.ced41.js',
+	settingName: 'src/settings.2dd2e.js',
+	mainName: 'main.c8cfd.js',
 	cocos2djsName: 'cocos2d-js-min.63557.js',
 	physicsName: 'physics-min.js'
 };
