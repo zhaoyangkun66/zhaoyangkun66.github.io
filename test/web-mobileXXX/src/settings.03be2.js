28402a3bbb511ed1d38fe60fe4658afa1aaa596a
zhaoyangkun66 <569188417@qq.com>
2026-04-21 23:15:11 +0800

    1

--- test/web-mobileXXX/src/settings.03be2.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:["assets/Script/NewScript.07663.js"],bundleVers:{internal:"d17e2",main:"0688e"}};
